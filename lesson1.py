3c558e37c0b71cf5dac939a4c6a6a5036ccd74dc
Andrew2341 <sir.lomonosov98@gmail.com>
2022-08-29 22:23:05 +0300

    first-lesson-done (elif, for, while)

---
+++ lesson1.py
@@ -0,0 +1,39 @@
+#my_int = '5'
+#my_another_int = 30
+#print(my_int + str(my_another_int))
+
+#print("Введите температуру в вашем городе \n fhfh")
+#my_weather = int(input())
+#if my_weather < 10:
+#    print('оденься потеплее')
+#elif my_whether > 25:
+#    print('надо выходить в трусах')
+#else:
+#    print('на улице норм')
+
+#arr = [1, ["раз", "два", "три"], '3', 4]
+#print(arr)
+#arr.remove("3")
+#print(arr)
+#arr[1].remove("два")
+#print(arr)
+#arr.append("супер")
+#print(arr)
+
+#for index in range(0, 10):
+#    print(index)
+
+#for i in "privet":
+#    print(i)
+
+#arr = [1, ["раз", "два", "три"], 3, 4]
+#for i in arr:
+#    print(i)
+
+my_value = 10
+i = 0
+while i < my_value:
+    print(i)
+    if i == 5:
+        break
+    i = i + 1
